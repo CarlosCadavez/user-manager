ece73c3665c9f7649299af0616c9db66c17bc82f
CarlosCadavez <cs.cadavez@gmail.com>
2021-06-25 18:55:02 -0300

    feat: add entities mongo

---
+++ user.core/src/main/java/br/com/cscdesenvolvimento/bank/user/core/models/Account.java
@@ -0,0 +1,21 @@
+package br.com.cscdesenvolvimento.bank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collation = "accounts")
+public class Account {
+
+    private String username;
+    private String password;
+    private List<Role> roles;
+}
