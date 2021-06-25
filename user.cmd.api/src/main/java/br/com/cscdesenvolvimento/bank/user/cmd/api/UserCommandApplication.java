194286f9bfa9541e568159cbd04a32957db3df5a
CarlosCadavez <cs.cadavez@gmail.com>
2021-06-25 18:12:32 -0300

    feat: add axon mongo integration

--- user.cmd.api/src/main/java/br/com/cscdesenvolvimento/bank/user/cmd/api/UserCommandApplication.java
+++ user.cmd.api/src/main/java/br/com/cscdesenvolvimento/bank/user/cmd/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package br.com.cscdesenvolvimento.bank.user.cmd.api;
 
+import br.com.cscdesenvolvimento.bank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class UserCommandApplication {
 
 	public static void main(String[] args) {
--- user.query.api/src/main/java/br/com/cscdesenvolvimento/bank/user/query/api/UserQueryApplication.java
+++ user.query.api/src/main/java/br/com/cscdesenvolvimento/bank/user/query/api/UserQueryApplication.java
@@ -1,9 +1,12 @@
 package br.com.cscdesenvolvimento.bank.user.query.api;
 
+import br.com.cscdesenvolvimento.bank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class UserQueryApplication {
 
 	public static void main(String[] args) {
