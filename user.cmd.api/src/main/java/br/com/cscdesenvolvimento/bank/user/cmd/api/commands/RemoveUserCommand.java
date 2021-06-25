659387381c558b815cb398369b20b5bd78ff49e3
CarlosCadavez <cs.cadavez@gmail.com>
2021-06-25 19:36:11 -0300

    refactor: deleted attribute

--- user.cmd.api/src/main/java/br/com/cscdesenvolvimento/bank/user/cmd/api/commands/RemoveUserCommand.java
+++ user.cmd.api/src/main/java/br/com/cscdesenvolvimento/bank/user/cmd/api/commands/RemoveUserCommand.java
@@ -1,11 +1,9 @@
 package br.com.cscdesenvolvimento.bank.user.cmd.api.commands;
 
-import br.com.cscdesenvolvimento.bank.user.core.models.User;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public class RemoveUserCommand {
 
     @TargetAggregateIdentifier
     private String id;
-    private User user;
 }
